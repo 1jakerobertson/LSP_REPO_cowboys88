3c9dea158a5a8da7f62c79a8d58f9473c6260ef0
1jakerobertson <jacob.robertson@bison.howard.edu>
2025-08-19 14:33:48 -0400

    Class assignment1

---
+++ org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,9 @@
+/* Jacob Robertson */
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
